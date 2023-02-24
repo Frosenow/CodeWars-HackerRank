bd3f0883b6cfaa83b9d642a8fe9b0e9e6fbff0fc
Frosenow <filip.potepa@gmail.com>
2023-02-24 16:11:15 +0100

    Create Migratory-Birds.js

---
+++ HackerRank/Migratory-Birds.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'migratoryBirds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function migratoryBirds(arr) {
+    let frequency = 1; 
+    let max = 0;
+    let id; 
+    arr.sort((a, b) => a - b).forEach((bird, idx, birds) => {
+        if(bird == birds[idx + 1]){
+             frequency++;
+        } else {
+            if(frequency > max){
+                max = frequency
+                id = bird; 
+                frequency = 1; 
+            }
+        }
+    })
+    console.log(arr)
+    return id
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arrCount = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = migratoryBirds(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
