77d983f4885a86e5fa71a3d759798a237a7639a0
Frosenow <filip.potepa@gmail.com>
2023-03-04 22:37:31 +0100

    Create Human-Readable-Duration-Format.js

---
+++ CodeWars/4th Kyu/Human-Readable-Duration-Format.js
@@ -0,0 +1,39 @@
+function formatDuration (time) {
+  if(time == 0) return 'now'
+  let years, days, hours, minutes, seconds; 
+  years = days = hours = minutes = seconds = 0;
+  
+  const clock = { 
+    year: 0,
+    day: 0,
+    hour: 0,
+    minute: 0,
+    second: 0,
+  }
+    clock.second += time % 60; 
+      time -= clock.second;
+    
+    clock.minute += (Math.floor(time / 60) % 60)
+      time -= (clock.minute * 60)
+    
+    clock.hour += (Math.floor(time / 3600) % 3600) % 24
+      time -= (clock.hour * 3600)
+    
+    clock.day += Math.floor(time / (3600 * 24)) % 365
+      time -+ (clock.day * 3600 * 24)
+    
+    clock.year += Math.floor(time / (3600 * 24 * 365))
+      time -= (clock.year * 3600 * 24 * 365)
+    
+  let humanReadable = [];
+  for(const key in clock){
+    if(clock[key] > 0){
+      clock[key] > 1 ? humanReadable.push(`${clock[key]} ${key}s`) : humanReadable.push(`${clock[key]} ${key}`) 
+    }
+  }
+  humanReadable = humanReadable.join(', ')
+  if(humanReadable.includes(',')){
+    humanReadable = humanReadable.substring(0, humanReadable.lastIndexOf(',')) + ' and ' + humanReadable.substring(humanReadable.lastIndexOf(',') + 2)
+  }
+  return humanReadable
+}
