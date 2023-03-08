cb8a2d20d5dfcb8814da34b95f96d9b29f973d4a
Frosenow <filip.potepa@gmail.com>
2023-03-08 21:39:59 +0100

    Create Number-Line-Jumps.js

---
+++ HackerRank/Number-Line-Jumps.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'kangaroo' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER x1
+ *  2. INTEGER v1
+ *  3. INTEGER x2
+ *  4. INTEGER v2
+ */
+
+function kangaroo(x1, v1, x2, v2) {
+    // y = ax + b where a = v b = x
+    let flag = 'NO'
+    
+    // Generate arguments for both functions, to check if they cross 
+    for(let step = 0; step < 10000; step++){
+      let y1 = Math.round(v1 * step + x1) 
+      let y2 = Math.round(v2 * step + x2)
+      if(y1 == y2){
+          flag = 'YES'
+          break;
+      }  
+    } 
+    return flag; 
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const x1 = parseInt(firstMultipleInput[0], 10);
+
+    const v1 = parseInt(firstMultipleInput[1], 10);
+
+    const x2 = parseInt(firstMultipleInput[2], 10);
+
+    const v2 = parseInt(firstMultipleInput[3], 10);
+
+    const result = kangaroo(x1, v1, x2, v2);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
