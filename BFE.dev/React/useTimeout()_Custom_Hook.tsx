fe651254206c7890f4076bb345b48426deb8a0ae
Filip <filip.potepa@gmail.com>
2024-02-07 22:27:24 +0100

    Update useTimeout()_Custom_Hook.tsx

--- BFE.dev/React/useTimeout()_Custom_Hook.tsx
+++ BFE.dev/React/useTimeout()_Custom_Hook.tsx
@@ -1,3 +1,9 @@
+// PURPOSE: 
+// Create a hook to easily use setTimeout(callback, delay).
+
+// reset the timer if delay changes
+// DO NOT reset the timer if only callback changes
+
 import React, {useEffect, useRef} from 'react'
 export function useTimeout(callback: () => void, delay: number) {
 
@@ -11,10 +17,8 @@ export function useTimeout(callback: () => void, delay: number) {
   }, [delay])
 }
 
-// if you want to try your code on the right panel
-// remember to export App() component like below
-
 export function App() {
   useTimeout(() => console.log('test'), 1000)
   return <div>your app</div>
 }
+ 
