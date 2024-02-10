19abb11b41453ab67f41efe9ad440fb22dd15626
Filip <filip.potepa@gmail.com>
2024-02-10 22:09:54 +0100

    Update usePrevious()_Hook.tsx

--- BFE.dev/React/usePrevious()_Hook.tsx
+++ BFE.dev/React/usePrevious()_Hook.tsx
@@ -1,5 +1,7 @@
-import React, {useRef, useEffect} from 'react'
+import React, {useRef, useEffect, useMemo} from 'react'
+// PURPOSE: Create a hook usePrevious() to return the previous value, with initial value of `undefined.
 
+// useEffect Solution
 export function usePrevious<T>(value: T): T | undefined {
   const prevVal = useRef<T | undefined>(undefined)
 
@@ -8,8 +10,26 @@ export function usePrevious<T>(value: T): T | undefined {
   }, [value])
 
   return prevVal.current; 
-}
+} 
 
+// No-UseEffect Solution 
+export function usePrevious<T>(value: T): T | undefined {
+  const prevVal = useRef<T | undefined>(undefined)
+  const toReturn = prevVal.current; 
+  prevVal.current = value;  
+  return toReturn; 
+} 
+
+// useMemo Solution 
+export function usePrevious<T>(value: T): T | undefined {
+  const prevValue = useRef<T | undefined>(undefined)
+  
+  return useMemo(() => {
+    const prev = prevValue.current; 
+    prevValue.current = value; 
+    return prev; 
+  }, [value])
+}
 
 export function App() {
   return <div>your app</div>
